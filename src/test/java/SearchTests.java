9dd82960e1e47e7dc0215eb9488731cd0cce497f
Ivan Budenkov <ibudenkov@alfabank.ru>
2025-04-14 10:48:53 +0300

    init commit

---
+++ src/test/java/SearchTests.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SearchTests {
+    @Test
+    void duckDuckGoSearchTest() {
+        open("https://duckduckgo.com/");
+        $(".searchbox_input__rnFzM").setValue("selenide").pressEnter();
+        $(".Wo6ZAEmESLNUuWBkbMxx").shouldHave(text("https://selenide.org"));
+    }
+
+    @Test
+    void rumblerSearchTest() {
+        open("https://www.rambler.ru/");
+        $(".rc__86etrn").setValue("selenide").pressEnter();
+        $(".SmartCaptcha__title--yQxdI").shouldHave(text("Увы, что-то пошло не так"));
+    }
+}
