15c3fe352e0a0c8402230806bf7c89da09e375a8
Cree3gs <77207116+Cree3gs@users.noreply.github.com>
2025-04-14 11:28:25 +0300

    Update SearchTests.java

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -6,14 +6,14 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class SearchTests {
     @Test
-    void duckDuckGoSearchTest() {
+    void duckDuckGoSearchTest1() {
         open("https://duckduckgo.com/");
         $(".searchbox_input__rnFzM").setValue("selenide").pressEnter();
         $(".Wo6ZAEmESLNUuWBkbMxx").shouldHave(text("https://selenide.org"));
     }
 
     @Test
-    void rumblerSearchTest() {
+    void rumblerSearchTest2() {
         open("https://www.rambler.ru/");
         $(".rc__86etrn").setValue("selenide").pressEnter();
         $(".SmartCaptcha__title--yQxdI").shouldHave(text("Увы, что-то пошло не так"));
